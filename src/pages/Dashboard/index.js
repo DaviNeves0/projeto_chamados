f7d7fdd6193a5b040ef15512be4148601f07826c
Davi <davisilvaneves3@gmail.com>
2021-07-18 12:59:29 -0300

    Adicão do Componente Modal

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,7 @@
 import './dashboard.css';
 import Header from '../../components/Header'
 import Title from '../../components/Title';
+import Modal from '../../components/Modal';
 
 
 import { FiMessageSquare,FiPlusCircle,FiSearch, FiEdit2 } from 'react-icons/fi'
@@ -9,7 +10,7 @@ import {Link} from 'react-router-dom'
 import {format} from 'date-fns';
 import { useState, useEffect } from 'react';
 import firebase from '../../services/firebaseConnection';
-import { doc } from 'prettier';
+
 
 const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc');
 
@@ -20,14 +21,14 @@ export default function Dashboard(){
     const [isEmpty, setIsEmpty] = useState(false);
     const [lastDocs, setLastDocs] = useState();
 
+    //modal
+    const [showPostModal, setShowPostModal] = useState(false);
+    const [detail, setDetail] = useState([]);
+
     useEffect(()=>{
         loadChamados();
-        return () =>{
-
-        }
-    }, []);
-
-    async function loadChamados(){
+        
+        async function loadChamados(){
         await listRef.limit(5)
         .get()
         .then((snapshot)=>{
@@ -39,6 +40,12 @@ export default function Dashboard(){
         })
         setLoading(false);
     }
+        return () =>{
+
+        }
+       
+    }, []);
+
 
     async function updateState(snapshot){
         const isCollectionEmpty = snapshot.size === 0;
@@ -51,7 +58,8 @@ export default function Dashboard(){
                     assunto: doc.data().assunto,
                     cliente: doc.data().cliente,
                     clienteId: doc.data().clienteId,
-                    created: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+                    created: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
                     status: doc.data().status,
                     complemento: doc.data().complemento
 
@@ -61,11 +69,42 @@ export default function Dashboard(){
             const lastDoc = snapshot.docs[snapshot.docs.length -1];
             setChamados(chamados => [...chamados, ...lista]);
             setLastDocs(lastDoc);
+            console.log(chamados)
 
         }else{
             setIsEmpty(true);
         }
         setLoadingMore(false);
+    }
+
+   async function handleMore(){
+        setLoadingMore(true);
+        await listRef.startAfter(lastDocs).limit(5)
+        .get()
+        .then((snapshot)=>{
+            updateState(snapshot)
+        })
+    }
+
+    function togglePostModal(item){
+        setShowPostModal(!showPostModal); //troca True para False
+        setDetail(item);
+    }
+
+    if(loading){
+        return(
+           <div> 
+            <Header/>
+            <div className="content">
+            <Title name="Dashboard">
+                <FiMessageSquare size={25}/>
+            </Title>
+            <div className="container dashboard">
+                <span>Buscando Chamados...</span>
+            </div>
+            </div>
+            </div>
+        )
     }
 
     return(
@@ -101,29 +140,43 @@ export default function Dashboard(){
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td data-label="Cliente">Sujeito</td>
-                                    <td data-label="Assunto">Suporte</td>
-                                    <td data-label="Status">
-                                        <span className="badge" style={{backgroundColor:'#5cb85c'}}>Em aberto</span>
-                                    </td>
-                                    <td data-label="Cadastrado">20/06/2021</td>
-                                    <td data-label="#">
-                                        <button className="action" style={{backgroundColor: '#3583f6'}}>
-                                            <FiSearch color="#fff" size={17}/>
-                                        </button>
-                                        <button className="action" style={{backgroundColor: '#F6a935'}}>
-                                            <FiEdit2 color="#fff" size={17}/>
-                                        </button>
-                                    </td>
-                                </tr>
+                                {chamados.map((item, index)=>{
+                                    return(
+                                        <tr key={index}>
+                                        <td data-label="Cliente">{item.cliente}</td>
+                                        <td data-label="Assunto">{item.assunto}</td>
+                                        <td data-label="Status">
+                                            <span className="badge" style={{backgroundColor: item.status === 'Aberto' ? '#5cb85c' : '#999'}}>{item.status}</span>
+                                        </td>
+                                        <td data-label="Cadastrado">{item.createdFormated}</td>
+                                        <td data-label="#">
+                                            <button className="action" style={{backgroundColor: '#3583f6'}} onClick={() => togglePostModal(item) }>
+                                                <FiSearch color="#fff" size={17}/>
+                                            </button>
+                                            <Link className="action" style={{backgroundColor: '#F6a935'}} to={`/new/${item.id}`}>
+                                                <FiEdit2 color="#fff" size={17}/>
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                    )
+                                })}
+                                
                             </tbody>
                         </table>
+                        {loadingMore && <h3 style={{textAlign: 'center', marginTop: 15}}>Buscando dados...</h3>}
+                        {!loadingMore && !isEmpty && <button className="btn-more" onClick={handleMore}>Buscar mais</button>}
                     </>
                 )}
                 
                 
             </div>
+            
+            {showPostModal && (
+                <Modal
+                conteudo = {detail}
+                close = {togglePostModal}
+                />
+            )}
            
         </div>
     )
